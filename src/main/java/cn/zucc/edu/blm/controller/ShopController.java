da15181750acb136e5f74c996d92624ea982cc9b
叶力 <304363641@stu.zucc.edu.cn>
2019-12-02 20:19:12 +0800

    update some api

---
+++ src/main/java/cn/zucc/edu/blm/controller/ShopController.java
@@ -0,0 +1,44 @@
+package cn.zucc.edu.blm.controller;
+
+
+import cn.zucc.edu.blm.Dao.ShopDao;
+import cn.zucc.edu.blm.bean.Shop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/Shop")
+public class ShopController {
+    @Autowired
+    private ShopDao shopDao;
+
+    @GetMapping("/getShopList")
+    public List<Shop> getShopList(){
+        List lst=new ArrayList();
+        lst=shopDao.findAll();
+        return  lst;
+    }
+
+    @GetMapping("/getShopIdList")
+    public List<Integer> getShopIdList(){
+        List<Shop> lst=new ArrayList<Shop>();
+        List<Integer> response=new ArrayList<>();
+        lst=shopDao.findAll();
+        for(Shop a:lst){
+            response.add(a.getShopId());
+        }
+
+        return response;
+    }
+
+    @GetMapping("/getShopById")
+    public Shop getShopById(@RequestParam(value = "shopId") int shopId){
+        return shopDao.findById(shopId).orElse(null);
+    }
+}
